07e681fa804186791cdb60cbbf881af9f04ef5d1
David Ranich <david-mac-mini@Davids-Mac-mini.local>
2025-04-08 00:12:54 -0400

    Made the map a little more responsive

--- src/components/EventMap.vue
+++ src/components/EventMap.vue
@@ -1,5 +1,5 @@
 <template>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d483.94208128945667!2d-82.90809133186507!3d42.47959456603355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824d7fd60a123c5%3A0x8fdf07c141845b42!2sKnights%20of%20Columbus!5e0!3m2!1sen!2sus!4v1743888825822!5m2!1sen!2sus" width="" height="" style="bxorder: 1px solid #fff;" class="w-110 h-110 juxstify-center drop-shadow-lg rounded" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d483.94208128945667!2d-82.90809133186507!3d42.47959456603355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824d7fd60a123c5%3A0x8fdf07c141845b42!2sKnights%20of%20Columbus!5e0!3m2!1sen!2sus!4v1743888825822!5m2!1sen!2sus" width="" height="" style="" class="w-89 md:w-110 md:h-110 drop-shadow-lg rounded" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </template>
   
 <script setup>

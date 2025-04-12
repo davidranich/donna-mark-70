acd05801b7ddf8372189a7f2e173e89730a9528c
David Ranich <david-mac-mini@Davids-Mac-mini.local>
2025-04-12 03:23:26 -0400

    Simplified photos logic and usage

--- src/components/PhotosCarousel.vue
+++ src/components/PhotosCarousel.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="flex flex-col items-center w-full">
       <Carousel v-bind="carouselConfig" class="w-full max-w-[90vw] sm:max-w-xl md:max-w-2xl">
-        <Slide v-for="photo in photos" :key="photo.id" class="flex justify-center items-center">
+        <Slide v-for="photo in photos" class="flex justify-center items-center">
           <img
-            :src="photo.url"
+            :src="photo"
             class="rounded-xl shadow-lg object-cover max-h-[400px] w-full sm:w-auto"
-            alt="Party Photo"
           />
         </Slide>
       </Carousel>
@@ -32,22 +31,22 @@
     transition: 1000,
     pauseAutoplayOnHover: true
   };
-  
-  const photos = [
-    { id: 5, url: img4120 },
-    { id: 7, url: img6011 },
-    { id: 8, url: img6012 },
-    { id: 9, url: img5025 },
-    { id: 10, url: img9980 },
-    { id: 11, url: img1558 },
-    { id : 12, url: img3608 }
+
+const photos = [
+    img4120,
+    img6011,
+    img6012,
+    img5025,
+    img9980,
+    img1558,
+    img3608
   ];
-  </script>
+</script>
   
-  <style scoped>
+<style scoped>
   img {
     max-height: 400px;
     object-fit: cover;
     border-radius: 0.75rem;
   }
-  </style>
+</style>

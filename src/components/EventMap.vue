71947cada4fed393877f7716cd3a2efd822f87fb
David Ranich <david-mac-mini@Davids-Mac-mini.local>
2025-04-12 04:05:29 -0400

    Added vue google maps component into main.js, and modified existing component to utilize that component

--- src/components/EventMap.vue
+++ src/components/EventMap.vue
@@ -1,6 +1,127 @@
 <template>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d483.94208128945667!2d-82.90809133186507!3d42.47959456603355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824d7fd60a123c5%3A0x8fdf07c141845b42!2sKnights%20of%20Columbus!5e0!3m2!1sen!2sus!4v1743888825822!5m2!1sen!2sus" width="" height="" style="" class="w-89 md:w-110 md:h-110 drop-shadow-lg rounded" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<GMapMap
+    :center="venueCoordinates"
+    :zoom="19"
+    :disableDefaultUI="true"
+    class="gmap"
+  >
+<GMapMarker
+    :position="venueMarker.position"
+    >
+    <GMapInfoWindow
+        :opened="true"
+        :closeclick="false"
+        class="flex"
+    >
+        <div class="text-center p-1 flex flex-col info-text items-center text-xs sm:text-base md:text-base">
+            <img class="w-20 sm:w-30 md:w-30 sm:max-w-xl md:max-w-2xl" src="https://upload.wikimedia.org/wikipedia/en/9/93/KoC_Logotype1.png">
+            <span class="font-bold text-xs" style="color: #edaa38;">Knights of Columbus</span>
+            <span class="map-link text-xs"><a :href="mapLink" target="_blank">25003 Little Mack Ave, St Clair Shores, MI 48080</a></span>
+            <span class="font-bold text-xs" style="color: #263876;">June 28th, 2025 @ 4:00 PM</span>
+        </div>
+    </GMapInfoWindow>
+</GMapMarker>
+    </GMapMap>
 </template>
   
 <script setup>
+import { onMounted } from 'vue';
+
+const address = "Knights of Columbus, 25003 Little Mack Ave, St Clair Shores, MI 48080";
+const encodedAddress = encodeURIComponent(address);
+
+// detect device and pick map platform
+const isAppleDevice = /iPad|iPhone|Macintosh|Mac|iPod/.test(navigator.userAgent);
+
+const mapLink = isAppleDevice
+  ? `http://maps.apple.com/?q=${encodedAddress}`
+  : `https://www.google.com/maps/search/?api=1&query=${encodedAddress}`;
+
+const venueCoordinates = {
+    lat: 42.47977001902156,
+    lng: -82.9077743008792
+};
+
+const venueMarker = {
+    position: {
+        lat: venueCoordinates.lat, lng: venueCoordinates.lng
+    }
+};
+
+const removeElX = () => {
+    setTimeout(() => {
+        let el = document.querySelector('.gm-style-iw-chr');
+        el.style.display = 'none';
+    }, 1000);
+};
+
+onMounted(() => {
+    removeElX();
+});
 </script>
+
+<style scoped>
+.info-text {
+    font-family: Mukta, sans-serif;
+}
+
+.map-link:hover {
+    cursor: pointer;
+    color: #263876;
+}
+
+@media (min-width: 10rem) {
+    .gmap {
+    width: 80vw; 
+    height: 60vw;
+    }
+}
+
+/* 'sm' */
+@media (min-width: 76rem) {
+    .gmap {
+    width: 80vw;
+    height: 39vw;
+    }
+}
+
+/* 'xl' */
+@media (min-width: 80rem) {
+    .gmap {
+    width: 65vw; 
+    height: 20vw;
+    }
+}
+
+/* '2xl' */
+@media (min-width: 96rem) {
+    .gmap {
+    width: 50vw; 
+    height: 20vw;
+    }
+}
+
+/* '3xl' */
+@media (min-width: 112rem) {
+    .gmap {
+    width: 50vw;
+    height: 20vw;
+    }
+}
+
+/* '4xl' */
+@media (min-width: 128rem) {
+    .gmap {
+    width: 42vw; 
+    height: 20vw;
+    }
+}
+
+/* '5xl' */
+@media (min-width: 144rem) {
+    .gmap {
+    width: 37vw; 
+    height: 20vw;
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './style.css';
+import VueGoogleMaps from '@fawmi/vue-google-maps';
 
 const app = createApp(App);
+
+app.use(VueGoogleMaps, {
+    load: {
+        key: import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+    },
+});
 
 app.mount('#app');

c429f5abb84041dfb48c48467a56fb6fc3d5a67d
David Ranich <david-mac-mini@Davids-Mac-mini.local>
2025-04-20 01:21:12 -0400

    Created page for login form to be used

---
+++ src/components/pages/LoginPage.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="flex flex-col items-center justify-center main-wrapper px-4 py-12">
+        <a href="/"><img :src="logo" class="logo"></a>
+    
+        <div v-if="!formSubmitted" class="flex flex-col gap-5">
+            <div>
+                <card>
+                    <template #body>
+                        <div class="max-w-4xl mx-auto flex flex-row sm:flex-col gap-2">
+                            <LoginForm />
+                        </div>
+                    </template>
+                </card>
+            </div>
+        </div>
+    </div>
+    </template>
+    
+    <script setup>
+    import { ref } from "vue";
+    import LoginForm from "../LoginForm.vue";
+    import Card from '../utility/Card.vue';
+    
+    import logo from '@/assets/donna-mark-vintage-02-70th-transparent.png';
+    
+    const formSubmitted = ref(false);
+    </script>
+    
+    <style scoped>
+    .logo {
+        width: 350px;
+        height: 350px;
+    }
+    
+    .subtitle {
+        font-family: "Julius Sans One", sans-serif;
+        letter-spacing: 6px;
+        font-weight: 300;
+        color: #B1C29E;
+    }
+    
+    .subtitle-surprise {
+        color: #edaa38;
+    }
+    
+    .card {
+        border-radius: 0.75em;
+        background-color: #dadfe1;
+    }
+    </style>

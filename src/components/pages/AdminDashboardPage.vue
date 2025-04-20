81032db8ea83fbd38eb0d7d24d48d4c96ad133dc
David Ranich <david-mac-mini@Davids-Mac-mini.local>
2025-04-20 01:23:06 -0400

    Created basic Administrator Dashboard page that houses the Guests Table. There will be more that will be added here, but this was the first thing that needed to be done

---
+++ src/components/pages/AdminDashboardPage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex flex-col justify-center items-center">
+        <a href="/"><img :src="logo" class="logo"></a>
+        <div class="text-center">
+            <span class="subtitle text-lg md:text-md">List Of Guests ({{ guestCount }})</span>
+            <GuestsTable @loaded="handleGuestLoad" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import GuestsTable from "../GuestsTable.vue";
+
+const guestCount = ref(0);
+const handleGuestLoad = (count) => {
+  guestCount.value = count;
+};
+
+import logo from '@/assets/donna-mark-vintage-02-70th-transparent.png';
+</script>
+
+<style scoped>
+.logo {
+    width: 350px;
+    height: 350px;
+}
+
+.subtitle {
+    font-family: "Julius Sans One", sans-serif;
+    letter-spacing: 6px;
+    font-weight: 500;
+    color: #B1C29E;
+}
+</style>
